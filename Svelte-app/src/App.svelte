79caff52679b1212b29b5432314bd6d468712b1b
ITmindCo <itmindco@gmail.com>
2020-03-17 17:09:43 +1000

    add Material UI

--- Svelte-app/src/App.svelte
+++ Svelte-app/src/App.svelte
@@ -1,9 +1,12 @@
 <script>
-	import Navbar from './Components/Navbar.svelte';
-	import Pages from './Components/Pages.svelte';
-	import Footer from './Components/Footer.svelte';
+  import Navbar from "./Components/Navbar.svelte";
+  import Pages from "./Components/Pages.svelte";
+  import Footer from "./Components/Footer.svelte";
+  import MyBtn from "./Components/myBtn.svelte";
+  
 </script>
 
+<MyBtn />
 <Navbar />
 <Pages />
 <Footer />
---
+++ Svelte-app/src/Components/myBtn.svelte
@@ -0,0 +1,21 @@
+<script>
+    import Button, { Label } from "@smui/button";
+</script>
+
+<style global type="text/scss">
+    /* button {
+        background-color: gold;
+    } */
+    @import '@smui/button/mixins';
+    $color: gold;
+  
+    @include smui-button__group-mdc-button--raised_($color); 
+</style>
+
+<div>
+    <p>hh</p>
+    <Button variant="unelevated" color="light">
+        <Label>Do Something</Label>
+    </Button>
+    <button class="blue">click</button>
+</div>
