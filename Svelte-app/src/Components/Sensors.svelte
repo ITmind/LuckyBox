f5e72e9a28b5b2814ba1503f39d3fad6d222aa77
ITmindCo <itmindco@gmail.com>
2020-03-30 20:17:53 +1000

    страница датчиков

--- Svelte-app/src/Components/Sensors.svelte
+++ Svelte-app/src/Components/Sensors.svelte
@@ -1,120 +1,166 @@
 <style>
+    section {
+        margin-left: 20px;
+        width: 768px;
+    }
+    p {
+        text-align: center;
+        font-weight: 700;
+    }
 
+    section > p {
+        text-align: center;
+        font-weight: 700;
+        margin: 10px 0px;
+    }
+    section > div {
+        display: grid;
+        grid-template-columns: 40px 2fr 70px 100px 1fr 1fr 1fr;
+    }
+
+    section > div:first-of-type {
+        border-bottom: 1px;
+        border-bottom-style: solid;
+        margin-bottom: 5px;
+    }
+
+    section > div > div:first-of-type {
+        display: grid;
+        grid-template-columns: 60px 1fr;
+        padding: 0px 10px;
+    }
+
+    section > div {
+        margin: 5px 0px;
+    }
+
+    section > div:hover {
+        background-color: rgb(250, 250, 250);
+    }
+
+    div div button {
+        border-radius: 0px;
+        background: white;
+    }
+    input, select {
+        background-color: transparent ;
+        outline: none !important;
+        border: none;
+        border-bottom-style: solid;
+        border-bottom-width: 1px;
+        border-bottom-color: gray;
+        justify-self: center;
+    }
+
+    input:focus {
+        border-bottom-width: 2px;
+        border-bottom-color: black;
+    }
 </style>
 
 <script>
     import { startInterval, stopInterval, globalSensorsJson } from "./../process.js";
     import Picker from "vanilla-picker"; //npm install vanilla-picker --save
+    import { onMount } from "svelte";
     let t_sensors = [1, 2, 3, 4, 5, 6, 7, 8];
     let devices = [1, 2, 3, 4];
     let safety = [1, 2, 3, 4];
 
-    function openPicker(e) {
-		console.log(e.target);
-        var picker = new Picker(e.target);
-        picker.onChange = function(color) {
-            e.target.style.background = color.rgbaString;
-        };
-    }
+    let thisElement;
+    //инициируем кнопки выбора цвета
+    onMount(() => {
+        var color_buttons = thisElement.querySelectorAll("div div button");
+        color_buttons.forEach(element => {
+            var picker = new Picker(element);
+            picker.onChange = function(color) {
+                element.style.background = color.rgbaString;
+            };
+        });
+    });
 </script>
 
 <!------------------------------------------->
 <!----------------MARKUP----------------------->
 <!------------------------------------------->
-<div class="container tab-pane theme_grey swipe-tab-content active">
+<section bind:this={thisElement}>
 
-    <div class="row row-xs mt-10" id="sensors_settings">
-        <div class="col-md-12 fill-height">
-            <div class="row row-xs text-center text-strong">
-                <p>Датчики температуры</p>
-            </div>
-            {#each t_sensors as sensor, i}
-                <div class="form-group row row-xs">
-                    <div class="col-xs-5 col-sm-5">
-                        <div class="input-group input-group-sm">
-                            <span class="input-group-addon" on:click={openPicker}>t{i}</span>
-                            <input placeholder="Название" id="sensor_name_t{i}" type="text" value="" />
-                        </div>
-                    </div>
-                    <div class="col-xs-2 col-sm-1">
-                        <input type="hidden" id="sensor_color_t1" onchange="svgFillUpdate(this.value,'t1')" value="" />
-                        <button class="btn btn-sm jscolor noSwipe" valueElement="null">Цвет</button>
-                    </div>
-                    <div class="col-xs-2 col-sm-3">
-                        <select id="sensor_number_t1" class="form-control input-sm noSwipe">
-                            <option value="">Сортировка</option>
-                            <option value="1">1</option>
-                            <option value="2">2</option>
-                            <option value="3">3</option>
-                            <option value="4">4</option>
-                            <option value="5">5</option>
-                            <option value="6">6</option>
-                            <option value="7">7</option>
-                            <option value="8">8</option>
-                        </select>
-                    </div>
-                    <div class="col-xs-3 col-sm-3">
-                        <span id="sensor_val_t1" />
-                        <span class="">&#176С</span>
-                    </div>
-                </div>
-            {/each}
+    <p>Датчики температуры</p>
+    <div>
+        <p>Цвет</p>
+        <p>Название</p>
+        <p>Сорт.</p>
+        <p>Значение</p>
+        <p>Дестиляция</p>
+        <p>Ратификация</p>
+        <p>Затирание</p>
 
-            <div class="form-group row row-xs">
-                <div class="col-xs-5 col-sm-5">
-                    <div class="input-group input-group-sm">
-                        <span class="input-group-addon">p1</span>
-                        <input id="sensor_name_p1" class="form-control input-sm noSwipe" type="text" readonly value="Атмосферное давление" />
-                    </div>
-                </div>
-                <div class="col-xs-2 col-sm-1">
-                    <input type="hidden" id="sensor_color_p1" value="" />
-                    <button class="btn btn-sm jscolor noSwipe">Цвет</button>
-                </div>
-                <div class="col-xs-2 col-sm-3" />
-                <div class="col-xs-3 col-sm-3">
-                    <span id="sensor_val_p1" />
-                    <span class="hidden">мм рт.ст.</span>
-                </div>
+    </div>
+    {#each t_sensors as sensor, i}
+        <div>
+
+            <button />
+            <input placeholder="t{i}" type="text" value={sensor.name ? sensor.name : ''} />
+
+            <select id="sensor_number_t1">
+                <option value="" />
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+            </select>
+
+            <div>
+                <span id="sensor_val_t1" />
+                <span class="">&#176С</span>
             </div>
 
-            <div class="row row-xs text-center text-strong">
-                <p>Исполнительные устройства</p>
-            </div>
-            {#each devices as device, i}
-                <div class="form-group row row-xs">
-                    <div class="col-xs-11 col-sm-5">
-                        <div class="input-group input-group-sm">
-                            <span class="input-group-addon">out{i}</span>
-                            <input placeholder="Название" id="device_name_out{i}" class="form-control input-sm noSwipe" type="text" value="" />
-                        </div>
-                    </div>
-                    <div class="col-xs-1 col-sm-7" />
-                </div>
-            {/each}
+            <input type=checkbox>
+            <input type=checkbox>
+            <input type=checkbox>
+        </div>
+    {/each}
 
-            <div class="row row-xs text-center text-strong">
-                <p>Датчики безопасности</p>
-            </div>
-            {#each safety as saf, i}
-                <div class="form-group row row-xs">
-                    <div class="col-xs-11 col-sm-5">
-                        <div class="input-group input-group-sm">
-                            <span class="input-group-addon">in{i}</span>
-                            <input placeholder="Название" id="safety_name_in{i}" class="form-control input-sm noSwipe" type="text" value="" />
-                        </div>
-                    </div>
-                    <div class="col-xs-1 col-sm-7" />
-                </div>
-            {/each}
+    <div>
 
-            <div class="form-group row row-xs" id="sensors_group_button">
-                <div class="col-xs-12">
-                    <button id="get_sensors" type="button" class="btn btn-primary btn-sm noSwipe mt-10">Опросить датчики</button>
-                    <button id="reset_sensors" type="button" class="btn btn-danger btn-sm noSwipe mt-10">Сбросить настройки</button>
-                    <button id="set_sensors" type="button" class="btn btn-success btn-sm noSwipe mt-10">Сохранить</button>
-                </div>
-            </div>
+        <button style="visibility: hidden;" />
+        <input id="sensor_name_p1" type="text" readonly value="Атмосферное давление" />
+
+        <div class="col-xs-2 col-sm-3" />
+        <div class="col-xs-3 col-sm-3">
+            <span id="sensor_val_p1" />
+            <span class="hidden">мм рт.ст.</span>
         </div>
     </div>
-</div>
+
+    <p>Исполнительные устройства</p>
+
+    {#each devices as device, i}
+        <div>
+
+            <button />
+            <input placeholder="out{i}" id="device_name_out{i}" type="text" value="" />
+
+        </div>
+    {/each}
+
+    <p>Датчики безопасности</p>
+
+    {#each safety as saf, i}
+        <div>
+            <button />
+            <input placeholder="in{i}" id="safety_name_in{i}" type="text" value="" />
+
+        </div>
+    {/each}
+
+    <div>
+        <button id="get_sensors" type="button" class="btn btn-primary btn-sm noSwipe mt-10">Опросить датчики</button>
+        <button id="reset_sensors" type="button" class="btn btn-danger btn-sm noSwipe mt-10">Сбросить настройки</button>
+        <button id="set_sensors" type="button" class="btn btn-success btn-sm noSwipe mt-10">Сохранить</button>
+    </div>
+
+</section>
